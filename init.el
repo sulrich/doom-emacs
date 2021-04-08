42fa99e281898b6ed53a84ae0e5f35a1f7386b28
steve ulrich <sulrich@botwerks.org>
2021-04-07 21:56:34 -0500

    added support for python +conda

--- init.el
+++ init.el
@@ -57,7 +57,8 @@
        ;; (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       (format +onsave)  ; automated prettiness
+       (format           ; automated prettiness
+        +onsave)
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        multiple-cursors  ; editing in many places at once
@@ -156,6 +157,7 @@
        ;;purescript        ; javascript, but functional
        (python            ; beautiful is better than ugly
         +pyenv
+        +conda
         +poetry
         +lsp)
        ;;qt                ; the 'cutest' gui framework ever
